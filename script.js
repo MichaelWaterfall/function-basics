8ea672b009e654f164fe33204919e6fdd3e8ccee
MichaelWaterfall <michaeljames.waterfall@gmail.com>
2025-06-05 07:30:04 +0100

    capitalize

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function add7(number){
 function multiply(num1, num2){
     return num1 * num2;
 }
+
+function capitalize(string){
+    lowerCase = string.toLowerCase();
+    upperCase = string[0].toUpperCase();
+    lowerCase = lowerCase.slice(1);
+    return upperCase + lowerCase;
+}
